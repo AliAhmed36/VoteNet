e0f20e6c9548fabe192e8b21bf41f3396c0d750d
Ulzhalgas Rakhman <11dulzhalgasr@gmail.com>
2022-06-12 02:30:51 +0900

    Update votenet.py

--- models/votenet.py
+++ models/votenet.py
@@ -17,8 +17,8 @@ BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 ROOT_DIR = os.path.dirname(BASE_DIR)
 sys.path.append(BASE_DIR)
 from backbone_module import Pointnet2Backbone
-from models.voting_mod import VotingModule
-from models.proposal_mod import ProposalModule
+from models.voting_ours import VotingModule
+from models.proposal_ours import ProposalModule
 from dump_helper import dump_results
 from loss_helper import get_loss
 
